b19500149d6e661374a37b8aaf323e41c81b12c8
Zakkurozu <afrizalahma5@gmail.com>
2025-02-24 14:34:22 +0700

    Routes add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import "./App.css";
 // import Navbar from "./layouts/Navbar";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import SingleProduct from "./pages/SingleProduct";
+import Footer from "./layouts/Footer";
 
 function App() {
   return (
     <>
       {/* <Navbar /> */}
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Shop" element={<Shop />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/SingleProduct" element={<SingleProduct />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
