1f3ed3b9368a1f9cbfc421586c892b19c21329ba
Zakkurozu <afrizalahma5@gmail.com>
2025-02-25 12:44:11 +0700

    navbar adjust

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -3,15 +3,30 @@ import { FaRegHeart } from "react-icons/fa";
 import { FiSearch, FiShoppingCart } from "react-icons/fi";
 import { LuUserCog } from "react-icons/lu";
 import { Link, useNavigate } from "react-router-dom";
+import Search from "../components/Search";
+import ShoppingCart from "../components/ShoppingCart";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const [isOpen, setIsOpen] = useState(false);
+  const [menuOpen, setmenuOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
+  const [cartOpen, setCartOpen] = useState(false);
   const menuRef = useRef(null);
+  const searchRef = useRef(null);
+  const cartRef = useRef(null);
   const buttonRef = useRef(null);
+  const searchBtnReff = useRef(null);
+  const cartBtnReff = useRef(null);
 
-  const handleOpen = () => {
-    setIsOpen((prev) => !prev);
+  const handleMenuOpen = () => {
+    setmenuOpen((prev) => !prev);
+  };
+  const handleSearchOpen = () => {
+    setSearchOpen((prev) => !prev);
+  };
+
+  const handleCartOpen = () => {
+    setCartOpen((prev) => !prev);
   };
 
   useEffect(() => {
@@ -22,20 +37,36 @@ const Navbar = () => {
         buttonRef.current &&
         !buttonRef.current.contains(event.target)
       ) {
-        setIsOpen(false);
+        setmenuOpen(false);
+      }
+      if (
+        searchRef.current &&
+        !searchRef.current.contains(event.target) &&
+        searchBtnReff.current &&
+        !searchBtnReff.current.contains(event.target)
+      ) {
+        setSearchOpen(false);
+      }
+      if (
+        cartRef.current &&
+        !cartRef.current.contains(event.target) &&
+        cartBtnReff.current &&
+        !cartBtnReff.current.contains(event.target)
+      ) {
+        setCartOpen(false);
       }
     };
-    if (isOpen) {
-      document.addEventListener("click", handleClickOutside);
+    if (menuOpen || searchOpen || cartOpen) {
+      document.addEventListener("click", handleClickOutside, true);
     }
     return () => {
-      document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("click", handleClickOutside, true);
     };
-  }, [isOpen]);
+  }, [menuOpen, searchOpen, cartOpen]);
 
   return (
     <>
-      <div className=" px-5 py-3 bg-terang">
+      <div className=" px-5 py-3 bg-putih">
         <div className="container w-full flex justify-between items-center relative">
           <div onClick={() => navigate("/")} className="flex gap-1 items-start">
             <img src="/logo.png" alt="" className="w-8 md:w-10" />
@@ -45,15 +76,19 @@ const Navbar = () => {
           </div>
           <div
             ref={menuRef}
-            className={`items-center gap-8 py-8 space-y-4 absolute w-[100%] overflow-hidden rounded-md bg-[#f4f4f4c2] 
+            className={`items-center gap-8 px-6 py-5 space-y-4 absolute w-[100%] overflow-hidden rounded-md bg-putih 
               translate-x-1/2 right-1/2 translate-y-[100%] -bottom-5 z-10 
               transition-transform origin-top duration-300 ease-in-out
-              ${isOpen ? "scale-y-100" : "scale-y-0"} 
-              md:translate-y-0 md:bottom-auto md:flex md:w-auto md:scale-y-100 md:space-y-0 md:py-0`}
+              ${menuOpen ? "scale-y-100" : "scale-y-0"} 
+              md:translate-y-0 md:bottom-auto md:flex md:w-auto md:scale-y-100 md:space-y-0 md:py-0 md:px-0`}
           >
+            <div className=" md:hidden">
+              <p className="font-semibold text-xl">Menu</p>
+              <hr />
+            </div>
             <div>
               <Link
-                onClick={() => setIsOpen(false)}
+                onClick={() => setmenuOpen(false)}
                 to="/"
                 className="flex w-full justify-center font-medium"
               >
@@ -62,7 +97,7 @@ const Navbar = () => {
             </div>
             <div>
               <Link
-                onClick={() => setIsOpen(false)}
+                onClick={() => setmenuOpen(false)}
                 to="/shop"
                 className="flex w-full justify-center font-medium"
               >
@@ -71,7 +106,7 @@ const Navbar = () => {
             </div>
             <div>
               <Link
-                onClick={() => setIsOpen(false)}
+                onClick={() => setmenuOpen(false)}
                 to="/about"
                 className="flex w-full justify-center font-medium"
               >
@@ -80,36 +115,69 @@ const Navbar = () => {
             </div>
             <div>
               <Link
-                onClick={() => setIsOpen(false)}
+                onClick={() => setmenuOpen(false)}
                 to="/contact"
                 className="flex w-full justify-center font-medium"
               >
                 Contact
               </Link>
             </div>
+            <hr className="md:hidden" />
           </div>
           <div className="flex gap-4 items-center">
             <button className="text-lg md:text-xl">
               <LuUserCog />{" "}
             </button>
-            <button className="text-lg md:text-xl">
+            <button
+              ref={searchBtnReff}
+              onClick={handleSearchOpen}
+              className="text-lg md:text-xl"
+            >
               <FiSearch />{" "}
             </button>
             <button className="text-lg md:text-xl">
               <FaRegHeart />{" "}
             </button>
-            <button className="text-lg md:text-xl">
+            <button
+              ref={cartBtnReff}
+              onClick={handleCartOpen}
+              className="text-lg md:text-xl"
+            >
               <FiShoppingCart />{" "}
             </button>
             <button
               ref={buttonRef}
-              onClick={handleOpen}
+              onClick={handleMenuOpen}
               className="flex flex-col gap-y-1 md:hidden"
             >
               <span className="w-[20px] h-[2px] bg-black"></span>
               <span className="w-[20px] h-[2px] bg-black"></span>
               <span className="w-[20px] h-[2px] bg-black"></span>
             </button>
+          </div>
+
+          {/* search open */}
+          <div
+            ref={searchRef}
+            className={`items-center gap-8 py-4 px-3 space-y-4 absolute w-[100%] overflow-hidden rounded-md bg-putih 
+            translate-x-1/2 right-1/2 translate-y-[100%] -bottom-5 z-10 
+            transition-transform origin-top duration-300 ease-in-out
+            ${searchOpen ? "scale-y-100" : "scale-y-0"} 
+            md:w-[60%] md:translate-x-0 md:right-0
+            lg:w-[40%]`}
+          >
+            <Search />
+          </div>
+
+          {/* cart open */}
+          <div
+            ref={cartRef}
+            className={`items-center gap-8 py-4 px-5 absolute w-[80%] overflow-hidden rounded-md bg-putih translate-x-0 right-0 translate-y-[100%] -bottom-5 z-10 transition-transform origin-top duration-300 ease-in-out shadow-sm
+            ${cartOpen ? "scale-y-100" : "scale-y-0"} 
+            md:w-[50%]
+            lg:w-[30%]`}
+          >
+            <ShoppingCart />
           </div>
         </div>
       </div>
