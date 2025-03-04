b040c8a1cb25c936e17e858ae7a89cc1be7b8804
Zakkurozu <afrizalahma5@gmail.com>
2025-03-04 15:18:40 +0700

    data dummy card (unused)

---
+++ src/data/cart.js
@@ -0,0 +1,20 @@
+export const cart = [
+  {
+    id: 1,
+    prodId: 5,
+    name: "Griffo",
+    img: "/products/prod5.png",
+    qty: 1,
+    price: 700000,
+  },
+  {
+    id: 2,
+    prodId: 13,
+    name: "Ludo Neo",
+    img: "https://media.istockphoto.com/id/1267156428/id/foto/kamar-tidur-hotel-dengan-lampu-baca-dan-jam-dengan-telepon-di-hotel-modern.jpg?s=612x612&w=0&k=20&c=eK-w4e3GN3Hldf1t8M-hFURhSvJ5R7BU2YL6JMXuLhU=",
+    qty: 2,
+    price: 516000,
+  },
+];
+
+export default cart;
