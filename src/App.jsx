52f5ad6ad6d9ada1949aaccceed7fe300574be4c
Zakkurozu <afrizalahma5@gmail.com>
2025-03-09 23:17:35 +0700

    progressbar component import

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Footer from "./layouts/Footer";
 import { useEffect } from "react";
 import SearchResult from "./pages/SearchResult";
 import CheckOut from "./pages/CheckOut";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import ProgressBar from "./components/ProgressBar";
 
 function App() {
   const location = useLocation();
@@ -31,9 +34,12 @@ function App() {
   return (
     <>
       <Navbar />
+      <ProgressBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/shop/product/:id/:name" element={<SingleProduct />} />
         <Route path="/search" element={<SearchResult />} />
