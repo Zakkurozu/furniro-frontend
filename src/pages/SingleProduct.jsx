67fe624053f49ee20f38f2860a846ce532505da0
Zakkurozu <afrizalahma5@gmail.com>
2025-03-11 11:40:34 +0700

    add skleleton laoding when user open single product page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -18,6 +18,7 @@ const SingleProduct = () => {
   const [descActive, setDescActive] = useState(true);
   const { pathname } = useLocation();
   const [alert, setAlert] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const updateItems = () => {
@@ -29,7 +30,10 @@ const SingleProduct = () => {
   }, []);
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0 });
+    setLoading(true);
+    const timeout = setTimeout(() => setLoading(false), 1000);
+    return () => clearTimeout(timeout);
   }, [pathname]);
 
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
@@ -101,18 +105,24 @@ const SingleProduct = () => {
                     modules={[FreeMode, Navigation, Thumbs]}
                     className="h-20 md:h-96 vertical-thumb-swiper"
                   >
-                    {imageLoop.map((item, index) => (
-                      <SwiperSlide
-                        key={index}
-                        className="cursor-pointer rounded overflow-hidden border-2 border-transparent hover:border-primary transition-all duration-300"
-                      >
-                        <img
-                          src={item}
-                          className="w-full h-full object-cover"
-                          alt={`Thumbnail ${index + 1}`}
-                        />
-                      </SwiperSlide>
-                    ))}
+                    {loading
+                      ? imageLoop.map((_, index) => (
+                          <SwiperSlide key={index}>
+                            <div className="w-full h-full bg-gray-400 animate-pulse rounded-md"></div>
+                          </SwiperSlide>
+                        ))
+                      : imageLoop.map((item, index) => (
+                          <SwiperSlide
+                            key={index}
+                            className="cursor-pointer rounded overflow-hidden border-2 border-transparent hover:border-primary transition-all duration-300"
+                          >
+                            <img
+                              src={item}
+                              className="w-full h-full object-cover"
+                              alt={`Thumbnail ${index + 1}`}
+                            />
+                          </SwiperSlide>
+                        ))}
                   </Swiper>
                 </div>
 
@@ -123,26 +133,35 @@ const SingleProduct = () => {
                     spaceBetween={10}
                     navigation={false}
                     thumbs={{ swiper: thumbsSwiper }}
-                    autoplay={{
-                      delay: 5000,
-                      disableOnInteraction: false,
-                      pauseOnMouseEnter: true,
-                    }}
+                    // autoplay={{
+                    //   delay: 5000,
+                    //   disableOnInteraction: false,
+                    //   pauseOnMouseEnter: true,
+                    // }}
                     modules={[FreeMode, Navigation, Thumbs, Autoplay]}
                     className="rounded-lg overflow-hidden shadow-lg h-80 md:h-96"
                   >
-                    {imageLoop.map((item, index) => (
-                      <SwiperSlide
-                        key={index}
-                        className="aspect-w-16 aspect-h-12"
-                      >
-                        <img
-                          src={item}
-                          className="w-full h-full object-cover"
-                          alt={`Nature ${index + 1}`}
-                        />
-                      </SwiperSlide>
-                    ))}
+                    {loading
+                      ? imageLoop.map((_, index) => (
+                          <SwiperSlide
+                            key={index}
+                            className="aspect-w-16 aspect-h-12"
+                          >
+                            <div className="w-full h-full bg-gray-400 animate-pulse rounded-md"></div>
+                          </SwiperSlide>
+                        ))
+                      : imageLoop.map((item, index) => (
+                          <SwiperSlide
+                            key={index}
+                            className="aspect-w-16 aspect-h-12"
+                          >
+                            <img
+                              src={item}
+                              className="w-full h-full object-cover"
+                              alt={`Nature ${index + 1}`}
+                            />
+                          </SwiperSlide>
+                        ))}
                   </Swiper>
                 </div>
               </div>
@@ -152,51 +171,79 @@ const SingleProduct = () => {
             <div className="w-full px-4 md:w-1/2">
               <div className="bg-white p-2 md:p-6">
                 <h1 className="text-2xl font-semibold text-gray-800 mb-4">
-                  {productDetail.name}
+                  {loading ? (
+                    <div className="w-[50%] h-8 bg-gray-400 animate-pulse rounded-md"></div>
+                  ) : (
+                    productDetail.name
+                  )}
                 </h1>
-                <div className="flex items-center mb-4">
-                  <span className="text-xl font-semibold text-gray4">
-                    Rp.{" "}
-                    {(
-                      productDetail.price -
-                      productDetail.price * (productDetail.discount / 100)
-                    ).toLocaleString("id-ID")}
-                  </span>
-                  {productDetail.discount > 0 && (
-                    <div className="flex items-center">
-                      <span className="ml-2 text-sm line-through text-gray-400">
-                        Rp.{productDetail.price.toLocaleString("id-ID")}
+                {loading ? (
+                  <div className="w-[70%] h-8 mb-3 bg-gray-400 animate-pulse rounded-md"></div>
+                ) : (
+                  <div className="flex items-center mb-4">
+                    <span className="text-xl font-semibold text-gray4">
+                      Rp.{" "}
+                      {(
+                        productDetail.price -
+                        productDetail.price * (productDetail.discount / 100)
+                      ).toLocaleString("id-ID")}
+                    </span>
+                    {productDetail.discount > 0 && (
+                      <div className="flex items-center">
+                        <span className="ml-2 text-sm line-through text-gray-400">
+                          Rp.{productDetail.price.toLocaleString("id-ID")}
+                        </span>
+                        <span className="ml-2 bg-red-100 text-red-600 text-xs px-2 py-1 rounded-full">
+                          {productDetail.discount}% OFF
+                        </span>
+                      </div>
+                    )}
+                    {productDetail.new && (
+                      <span className="ml-2 bg-green-200 text-green-600 text-xs px-2 py-1 rounded-full">
+                        New
                       </span>
-                      <span className="ml-2 bg-red-100 text-red-600 text-xs px-2 py-1 rounded-full">
-                        {productDetail.discount}% OFF
-                      </span>
+                    )}
+                  </div>
+                )}
+                <div className="flex flex-col gap-1 mb-4">
+                  {loading ? (
+                    <div className="w-[30%] h-6 bg-gray-400 animate-pulse rounded-md"></div>
+                  ) : (
+                    <div className="flex gap-2 text-gray4 font-medium">
+                      <p>Category : </p>
+                      <p>{productDetail.category}</p>
                     </div>
                   )}
-                  {productDetail.new && (
-                    <span className="ml-2 bg-green-200 text-green-600 text-xs px-2 py-1 rounded-full">
-                      New
-                    </span>
+                  {loading ? (
+                    <div className="w-[30%] h-6 bg-gray-400 animate-pulse rounded-md"></div>
+                  ) : (
+                    <div className="flex gap-2 text-gray4 font-medium">
+                      <p>Range : </p>
+                      <p>{productDetail.range}</p>
+                    </div>
+                  )}
+                  {loading ? (
+                    <div className="w-[30%] h-6 bg-gray-400 animate-pulse rounded-md"></div>
+                  ) : (
+                    <div className="flex gap-2 text-gray4 font-medium">
+                      <p>Tags : </p>
+                      <p>{productDetail.tag}</p>
+                    </div>
+                  )}
+                  {loading ? (
+                    <div className="w-[30%] h-6 bg-gray-400 animate-pulse rounded-md"></div>
+                  ) : (
+                    <div className="flex gap-2 text-gray4 font-medium">
+                      <p>Stock : </p>
+                      <p>{productDetail.stock}</p>
+                    </div>
                   )}
                 </div>
-                <div className="flex flex-col gap-1 mb-4">
-                  <div className="flex gap-2 text-gray4 font-medium">
-                    <p>Category : </p>
-                    <p>{productDetail.category}</p>
-                  </div>
-                  <div className="flex gap-2 text-gray4 font-medium">
-                    <p>Range : </p>
-                    <p>{productDetail.range}</p>
-                  </div>
-                  <div className="flex gap-2 text-gray4 font-medium">
-                    <p>Tags : </p>
-                    <p>{productDetail.tag}</p>
-                  </div>
-                  <div className="flex gap-2 text-gray4 font-medium">
-                    <p>Stock : </p>
-                    <p>{productDetail.stock}</p>
-                  </div>
-                </div>
-                <p className="text-gray-600 mb-4">{productDetail.overview}</p>
+                {loading ? (
+                  <div className="w-[90%] h-[4.5rem] mb-4 bg-gray-400 animate-pulse rounded-md"></div>
+                ) : (
+                  <p className="text-gray-600 mb-4">{productDetail.overview}</p>
+                )}
 
                 <div className="flex gap-3 fixed  bottom-0 left-0 bg-white shadow-[0px_-4px_10px_rgba(0,0,0,0.1)] px-3 py-3 z-10 w-full md:static md:shadow-none md:bg-transparent">
                   <div className="flex gap-5 justify-center items-center border-2 border-gray5 px-2 py-3 rounded-xl font-medium">
